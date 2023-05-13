d95934cce4a8e7518007271d4ff5cc7c13989a76
Hannan <najma.mahat@student.moringaschool.com>
2023-05-13 15:11:04 +0300

    Completed displaying associated data lab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  def show
+    user = find_user
+    render json: user, include: :items, status: :ok
+  end
+
+  private
+
+  def find_user
+    User.find(params[:id])
+  end
+
+  def not_found
+    render json: { error: "User not found" }, status: :not_found
+  end
+
 end
